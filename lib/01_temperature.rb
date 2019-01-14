4019ba128c29e9f088d260c4cb02bc83aa3ae8ee
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-14 10:40:25 +0300

    adding 01_temperature.rb

---
+++ lib/01_temperature.rb
@@ -0,0 +1,12 @@
+# Fahreiheit to ceilcus
+def ftoc(f_degree)
+    f_degree = f_degree
+    ceilsus = ((f_degree - 32) * 5) / 9
+    return ceilsus 
+end
+# Ceilcus to Fahreinheit
+def ctof(c_degree)
+    c_degree = c_degree
+    fahereinheit =  ( c_degree * 9 / 5) + 32
+    return fahereinheit
+end
